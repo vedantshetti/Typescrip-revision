28f7cf29594e6c481a3153fd1daf8f965292ef6f
vedantshetti <vedantshetti123456@gmail.com>
2025-06-25 13:49:47 +0530

    worked on the object of the print user

---
+++ 13.ts
@@ -0,0 +1,11 @@
+function printuser() : {name: string, age: number} {
+    return {
+        name: "John Doe",
+        age: 30
+    };
+}
+
+const user = printuser();
+console.log(user.name);
+console.log(user.age);
+console.log(user);
