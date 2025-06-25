144a2d4cb564db854ce08c95376dbefd9f59e6f9
vedantshetti <vedantshetti123456@gmail.com>
2025-06-25 13:25:28 +0530

    learned about the multi dimentional arrays

---
+++ 11.ts
@@ -0,0 +1,8 @@
+const singleDi: number[] = [1, 2, 3, 4, 5];
+const multiDi: number[][] = [[1, 2, 3, 4]];
+const tripleDi: number[][][] = [[[1, 2,3, 4]]];
+
+
+console.log(singleDi);
+console.log(multiDi);
+console.log(tripleDi);
