9e6140ff0a069a9f56cf48d66cc9dde1f7014ae2
vedantshetti <vedantshetti123456@gmail.com>
2025-06-25 13:29:30 +0530

    added the objects code

---
+++ 12.ts
@@ -0,0 +1,10 @@
+const person : {firstName: string, lastName: string, Age: number} = {
+    firstName: "John",
+    lastName: "Doe",
+    Age: 30
+}
+
+console.log(person.firstName);
+console.log(person.lastName);
+console.log(person.Age);
+console.log(person); 
