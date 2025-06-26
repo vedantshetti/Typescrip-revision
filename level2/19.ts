3dcc98a65605da79ed04dc2ea6c84abb070c631f
vedantshetti <vedantshetti123456@gmail.com>
2025-06-26 11:24:37 +0530

    practiced about the tuples

---
+++ level2/19.ts
@@ -0,0 +1,7 @@
+let myTuple : [string, number, boolean] = ['apple', 1, true];
+
+let [firstItem, secondItem, thirdItem] = myTuple;
+
+console.log(firstItem);
+console.log(secondItem);
+console.log(thirdItem);
