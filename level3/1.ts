5d768a0a2c6d260236e15fcd0a05462e531b88f9
vedantshetti <vedantshetti123456@gmail.com>
2025-06-26 12:23:19 +0530

    practiced the object learning about the classes

---
+++ level3/1.ts
@@ -0,0 +1,17 @@
+class Person {
+    readonly name: string;
+    readonly age: number;
+
+
+    constructor(name:string, age:number){
+        this.name = name;
+        this.age = age;
+    }
+
+
+}
+
+
+const person1 = new Person('Vedant', 20);
+ console.log(person1);
+ 
